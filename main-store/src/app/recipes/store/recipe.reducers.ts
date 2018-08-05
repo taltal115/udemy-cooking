104d25324c14511dea4545760bdbb6cc25c8845d
Tal David Shitrit <tal.s@screenpeaks.com>
2018-08-05 18:02:21 +0300

    recipe redux work

--- main-store/src/app/recipes/store/recipe.reducers.ts
+++ main-store/src/app/recipes/store/recipe.reducers.ts
@@ -1,7 +1,9 @@
 import {Recipe} from '../recipe.model';
 import {Ingredient} from '../../shared/ingredient.model';
+import * as RecipeActions from './recipe.actions';
+import * as fromApp from '../../store/app.reducers';
 
-export interface FeatureState {
+export interface FeatureState extends fromApp.AppState{
   recipes: State
 }
 
@@ -29,9 +31,39 @@ const initialState: State = {
   ]
 };
 
-export function recipeReducer(state = initialState, action) {
+export function recipeReducer(state = initialState, action: RecipeActions.RecipeActions) {
   switch(action.type) {
-
+    case (RecipeActions.SET_RECIPES):
+      return {
+        ...state,
+        recipes: [...action.payload]
+      };
+    case (RecipeActions.ADD_RECIPE):
+      return {
+        ...state,
+        recipes: [...state.recipes, action.payload]
+      };
+    case (RecipeActions.UPDATE_RECIPE):
+      const recipe = state.recipes[action.payload.index];
+      const updatedRecipe = {
+        ...recipe,
+        ...action.payload.updatedRecipe
+      };
+      const recipes = [...state.recipes];
+      recipes[action.payload.index] = updatedRecipe;
+      return {
+        ...state,
+        recipes: recipes
+      };
+    case (RecipeActions.DELETE_RECIPE):
+      const oldRecipes = [...state.recipes];
+      oldRecipes.splice(action.payload,1);
+      console.log('deleted with redux!');
+      return {
+        ...state,
+        recipes: oldRecipes
+      };
+    default:
+      return state;
   }
-  return state
 }
--- main-store/src/app/store/app.reducers.ts
+++ main-store/src/app/store/app.reducers.ts
@@ -9,5 +9,5 @@ export interface AppState {
 
 export const reducers: ActionReducerMap<AppState> = {
   shoppingList: fromShoppingList.shoppingListReducer,
-  auth: fromAuth.authReducer,
+  auth: fromAuth.authReducer
 };
